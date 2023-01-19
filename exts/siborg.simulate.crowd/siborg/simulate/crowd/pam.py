9ed5f0722e9e44bdd814f61298bf95452aecf843
cadop <cadop@users.noreply.github.com>
2023-01-19 11:08:26 -0500

    update pam to pep8

--- exts/siborg.simulate.crowd/siborg/simulate/crowd/pam.py
+++ exts/siborg.simulate.crowd/siborg/simulate/crowd/pam.py
@@ -14,76 +14,54 @@ from scipy.spatial import distance
 class Parameters:
 
     # The agents field of view
-    fieldOfView = 200.0
-    
+    field_of_view = 200.0
     # The agents radius ? Used here in this implementation or in sim?
-    agentRadius = 0.5
-    
+    agent_radius = 0.5
     # Minimum agent distance
-    minAgentDistance = 0.1
-    
+    min_agent_dist = 0.1
     # the mid distance parameters in peicewise personal space function predictive force dist
     dmid = 4.0
-    
     # KSI  
     ksi = 0.5
-    
     # Nearest Neighbour distance ? Used here in this implementation or in sim?
-    neighborDist = 10.0
-    
+    neighbor_dist = 10.0
     # Maximum neighbours to consider ? Used here in this implementation or in sim?
-    maxNeighbors = 3
-    
+    max_neighbors = 3
     # Maximum acceleration ? Used here in this implementation or in sim/physics?
-    maxAccel = 20.0
-    
+    max_accel = 20.0
     # Maximum speed
-    maxSpeed = 2.0
-    
+    max_speed = 2.0
     # Preferred Speed
-    preferredSpeed = 1.3
-    
+    preferred_vel = 1.3
     # Goal acquired radius 
-    goalRadius = 1.0
-    
+    goal_radius = 1.0
     # Time Horizon
-    timeHorizon = 4.0
-    
+    time_horizon = 4.0
     # Agent Distance
-    agentDistance = 0.1
-    
+    agent_dist = 0.1
     # Wall Distance
-    wallDistance = 0.1
-    
+    wall_dist = 0.1
     # Wall Steepnes
-    wallSteepness = 2.0
-    
+    wall_steepness = 2.0
     # Agent Strength
-    agentStrength = 1.0
-    
+    agent_strength = 1.0
     # wFactor, factor to progressively scale down forces in when in a non-collision state
-    wFactor = 0.8
-    
+    w_factor = 0.8
     # Noise flag (should noise be added to the movement action)
-    noise = False
+    noise = False 
     
     # *private* Ideal wall distance
-    idealWallDistance = agentRadius + wallDistance
-    
+    _ideal_wall_dist = agent_radius + wall_dist
     # *private* Squared ideal wall distance
-    SAFE = idealWallDistance * idealWallDistance
-    
+    _SAFE = _ideal_wall_dist * _ideal_wall_dist
     # *private* Agent Personal space
-    agentPersonalSpace = agentRadius + minAgentDistance
-
+    _agent_personal_space = agent_radius + min_agent_dist
     # *private* the min distance parameters in peicewise personal space function
-    dmin = agentRadius + agentPersonalSpace
-    
+    _dmin = agent_radius + _agent_personal_space
     # *private* the max distance parameters in peicewise personal space function
-    dmax = timeHorizon * maxSpeed
-
+    _dmax = time_horizon * max_speed
     # *private* FOV cosine
-    cosFOV = np.cos((0.5 * np.pi * fieldOfView) / 180.0)
+    _cosFOV = np.cos((0.5 * np.pi * field_of_view) / 180.0)
     
 def ray_intersects_disc(pi, pj, v, r):
     # calc ray disc est. time to collision
@@ -131,45 +109,40 @@ def get_neighbors(cur, agents, pn_r):
 def compute_force(rr_i, ri, vv_i, mass, goal, pn_rr, pn_vv, pn_r, dt):
     # will store a list of tuples, each tuple is (tc, agent)
     t_pairs = []
-    forceCount = 0
+    force_count = 0
     
     # Preferred velocity is preferred speed in direction of goal
-    preferredVelocity = Parameters.preferredSpeed * norm(goal - rr_i)
+    preferred_vel = Parameters.preferred_vel * norm(goal - rr_i)
     
     # Goal force, is always added
-    goalForce = (preferredVelocity - vv_i) / Parameters.ksi
+    goal_force = (preferred_vel - vv_i) / Parameters.ksi
     
     # Desuired values if all was going well in an empty world
-    desiredVelocity = vv_i + goalForce * dt
-    desiredSpeed = mag(desiredVelocity)
+    desired_vel = vv_i + goal_force * dt
+    desired_speed = mag(desired_vel)
 
     # Handle obstacles
-    obstacleForce = np.array([0, 0, 0], dtype='float64')
-    '''
-    for (int i = 0; i < obstacles.Length; ++i)
-    {
+    obstacle_force = np.array([0, 0, 0], dtype='float64')
+    
+    # for i in range(len(obstacles)):
         #Step 1: get closest point on obstacle to agent
         #[[ Need python code for this in simulation ]]
         
-        Vector3 n_w = transform.position - closestPoint;
+        # n_w = rr_i - closest_point
+        # d_w = mag(n_w) * mag(n_w)
 
-        float d_w = n_w.sqrMagnitude;
+        # if (d_w < SAFE):
+        #     d_w = np.sqrt(d_w)
+        #     if (d_w > 0):
+        #         n_w /= d_w
 
-        if (d_w < SAFE)
-        {
-            d_w = Mathf.Sqrt(d_w);
-            if (d_w > 0)
-                n_w /= d_w;
-
-            float distanceMinimumRadius = ((d_w - agentRadius) < 0.001f) ? 0.001f : d_w - agentRadius;
-            Vector3 obstacleForce = (idealWallDistance - d_w) / Mathf.Pow(distanceMinimumRadius, wallSteepness) * n_w;
-            rb.AddForce(obstacleForce, ForceMode.Force);
-        }
-    }
-    '''
+        #     distanceMinimumRadius = ((d_w - Parameters.agent_radius) < 0.001f) ? 0.001f : d_w - Parameters.agent_radius
+        #     obstacle_force = (Parameters._ideal_wall_dist - d_w) / np.Pow(distanceMinimumRadius, Parameters.wall_steepness) * n_w
+        #     add_force(obstacle_force)
+    
     
     # Keep track of if we ever enter a collision state
-    agentCollision = False
+    agent_collision = False
     
     # Handle agents tc values for predictive forces among neighbours
     for j, rr_j in enumerate(pn_rr):
@@ -179,66 +152,66 @@ def compute_force(rr_i, ri, vv_i, mass, goal, pn_rr, pn_vv, pn_r, dt):
         #  Get radii of neighbor agent
         rj = pn_r[j]
         
-        combinedRadius = Parameters.agentPersonalSpace + rj
+        combined_radius = Parameters._agent_personal_space + rj
         
         w = rr_j - rr_i 
-        if (mag(w) < combinedRadius):
-            agentCollision = True
+        if (mag(w) < combined_radius):
+            agent_collision = True
             t_pairs.append((0.0, j))
         else:
-            relDir = norm(w)
-            if np.dot(relDir, norm(vv_i)) < Parameters.cosFOV:
+            rel_dir = norm(w)
+            if np.dot(rel_dir, norm(vv_i)) < Parameters._cosFOV:
                 continue
                 
-            tc = ray_intersects_disc(rr_i, rr_j, desiredVelocity - vv_j, combinedRadius)
-            if tc < Parameters.timeHorizon:
-                if len(t_pairs) < Parameters.maxNeighbors:
+            tc = ray_intersects_disc(rr_i, rr_j, desired_vel - vv_j, combined_radius)
+            if tc < Parameters.time_horizon:
+                if len(t_pairs) < Parameters.max_neighbors:
                     t_pairs.append((tc, j))
                 elif tc < t_pairs[0][0]:
                     t_pairs.pop()
                     t_pairs.append((tc, j))
                     
     # This will be all the other forces, added in a particular way
-    drivingForce = np.array([0, 0, 0], dtype='float64')
+    driving_force = np.array([0, 0, 0], dtype='float64')
     
     # Handle predictive forces// Predictive forces
     for t_pair in t_pairs:
         # Nice variables from the t_pair tuples
         t = t_pair[0]
-        agentIndex = t_pair[1]
+        agent_idx = t_pair[1]
         
-        forceDirection = rr_i + (desiredVelocity * t) - pn_rr[agentIndex] - (pn_vv[agentIndex] * t)
-        forceDistance = mag(forceDirection)
-        if forceDistance > 0:
-            forceDirection /= forceDistance
+        force_dir = rr_i + (desired_vel * t) - pn_rr[agent_idx] - (pn_vv[agent_idx] * t)
+        force_dist = mag(force_dir)
+        if force_dist > 0:
+            force_dir /= force_dist
             
-        collisionDistance = np.maximum(forceDistance - Parameters.agentRadius - pn_r[agentIndex], 0.0)
+        collision_dist = np.maximum(force_dist - Parameters.agent_radius - pn_r[agent_idx], 0.0)
         
         #D = input to evasive force magnitude piecewise function
-        D = np.maximum( (desiredSpeed * t) + collisionDistance, 0.001)
+        D = np.maximum( (desired_speed * t) + collision_dist, 0.001)
         
-        forceMagnitude = 0.0
-        if D < Parameters.dmin:
-            forceMagnitude = Parameters.agentStrength * Parameters.dmin / D
+        force_mag = 0.0
+        if D < Parameters._dmin:
+            force_mag = Parameters.agent_strength * Parameters._dmin / D
         elif D < Parameters.dmid:
-            forceMagnitude = Parameters.agentStrength
-        elif D < Parameters.dmax:
-            forceMagnitude = Parameters.agentStrength * (Parameters.dmax - D) / (Parameters.dmax - Parameters.dmid)
+            force_mag = Parameters.agent_strength
+        elif D < Parameters._dmax:
+            force_mag = Parameters.agent_strength * (Parameters._dmax - D) / (Parameters._dmax - Parameters.dmid)
         else:
             continue
             
-        forceMagnitude *= np.power( (1.0 if agentCollision else Parameters.wFactor), forceCount)
-        forceCount += 1
-        drivingForce = forceMagnitude * forceDirection
+        force_mag *= np.power( (1.0 if agent_collision else Parameters.w_factor), force_count)
+        force_count += 1
+        driving_force = force_mag * force_dir
 
     # Add noise for reducing deadlocks adding naturalness
     if Parameters.noise:
         angle = np.random.uniform(0.0, 1.0) * 2.0 * np.pi
         dist = np.random.uniform(0.0, 1.0) * 0.001
-        drivingForce += dist * np.array([np.cos(angle),np.sin(angle),0], dtype='float64')
+        driving_force += dist * np.array([np.cos(angle),np.sin(angle),0], dtype='float64')
 
     # Clamp drivign force, 40 is arbitrary 
-    if mag(drivingForce) > 40.0:
-        drivingForce = norm(drivingForce) * 40.0
+    if mag(driving_force) > 40.0:
+        driving_force = norm(driving_force) * 40.0
     
-    return goalForce + obstacleForce + drivingForce
+    return goal_force + obstacle_force + driving_force
